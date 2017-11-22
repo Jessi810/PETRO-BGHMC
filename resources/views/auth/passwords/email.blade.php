e9fd234ca6cc5456d30e1bbf6decb021e457926d
Jessi <jessisibayan@gmail.com>
2017-11-22 10:16:58 +0800

    Changed forgot password UI (untested)
    Need to configure host's email to work.

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,100 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
+    <!-- Styles -->
+    <link href="{{ asset('modular/css/vendor.css') }}" rel="stylesheet">
+    <link href="{{ asset('modular/css/app.css') }}" rel="stylesheet">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <!-- Theme initialization -->
+    {{--  <script>
+        var themeSettings = (localStorage.getItem('themeSettings')) ? JSON.parse(localStorage.getItem('themeSettings')) :
+        {};
+        var themeName = themeSettings.themeName || '';
+        if (themeName)
+        {
+            document.write('<link rel="stylesheet" id="theme-style" href="css/app-' + themeName + '.css">');
+        }
+        else
+        {
+            document.write('<link rel="stylesheet" id="theme-style" href="css/app.css">');
+        }
+    </script>  --}}
+</head>
+<body>
+<div class="auth">
+    <div class="auth-container">
+        <div class="card">
+            <header class="auth-header">
+                <h1 class="auth-title">
+                    <div class="logo">
+                        <span class="l l1"></span>
+                        <span class="l l2"></span>
+                        <span class="l l3"></span>
+                        <span class="l l4"></span>
+                        <span class="l l5"></span>
+                    </div> PETRO-BGHMC </h1>
+            </header>
+            <div class="auth-content">
+                <p class="text-center">PASSWORD RECOVER</p>
+                <p class="text-muted text-center">
+                    <small>Enter your email address to recover your password.</small>
+                </p>
+                <form id="reset-form" action="{{ route('password.email') }}" method="POST" novalidate="">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control underlined" name="email" id="email" placeholder="Your email address" required> </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-block btn-primary">Reset</button>
+                    </div>
+                    <div class="form-group clearfix">
+                        <a class="pull-left" href="{{ route('login') }}">return to Login</a>
+                        <a class="pull-right" href="{{ route('register') }}">Sign Up!</a>
+                    </div>
+                </form>
             </div>
         </div>
+        {{--  <div class="text-center">
+            <a href="index.html" class="btn btn-secondary btn-sm">
+                <i class="fa fa-arrow-left"></i> Back to dashboard </a>
+        </div>  --}}
     </div>
 </div>
-@endsection
+<!-- Reference block for JS -->
+<div class="ref" id="ref">
+    <div class="color-primary"></div>
+    <div class="chart">
+        <div class="color-primary"></div>
+        <div class="color-secondary"></div>
+    </div>
+</div>
+<script>
+    (function(i, s, o, g, r, a, m)
+    {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function()
+        {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+    ga('create', 'UA-80463319-4', 'auto');
+    ga('send', 'pageview');
+</script>
+<script src="{{ asset('modular/js/vendor.js') }}"></script>
+<script src="{{ asset('modular/js/app.js') }}"></script>
+</body>
+</html>
