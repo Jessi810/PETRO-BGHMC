8c6e442b5e9319fb959555df1ab9f16a006c81ea
Jessi <jessisibayan@gmail.com>
2017-11-22 08:44:58 +0800

    Initialized ModularAdmin layout

---
+++ resources/views/layouts/modular.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('modular/css/vendor.css') }}" rel="stylesheet">
+    <link href="{{ asset('modular/css/app.css') }}" rel="stylesheet">
+
+    <!-- Theme initialization -->
+    {{--  <script>
+        var themeSettings = (localStorage.getItem('themeSettings')) ? JSON.parse(localStorage.getItem('themeSettings')) :
+        {};
+        var themeName = themeSettings.themeName || '';
+        if (themeName)
+        {
+            document.write('<link rel="stylesheet" id="theme-style" href="css/app-' + themeName + '.css">');
+        }
+        else
+        {
+            document.write('<link rel="stylesheet" id="theme-style" href="css/app.css">');
+        }
+    </script>  --}}
+</head>
+<body>
+<div class="main-wrapper">
+    <div class="app" id="app">
+        @include('includes.header')
+        @include('includes.sidebar')
+        <div class="sidebar-overlay" id="sidebar-overlay"></div>
+        <div class="sidebar-mobile-menu-handle" id="sidebar-mobile-menu-handle"></div>
+        <div class="mobile-menu-handle"></div>
+        <article class="content dashboard-page">
+            @yield('content')
+        </article>
+        @include('includes.footer')
+        @include('includes.modal')
+    </div>
+</div>
+
+<!-- Reference block for JS -->
+<div class="ref" id="ref">
+    <div class="color-primary"></div>
+    <div class="chart">
+        <div class="color-primary"></div>
+        <div class="color-secondary"></div>
+    </div>
+</div>
+
+<script>
+    (function(i, s, o, g, r, a, m)
+    {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function()
+        {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+    ga('create', 'UA-80463319-4', 'auto');
+    ga('send', 'pageview');
+</script>
+<script src="{{ asset('modular/js/vendor.js') }}"></script>
+<script src="{{ asset('modular/js/app.js') }}"></script>
+</body>
+</html>
--- resources/views/trainer/index.blade.php
+++ resources/views/trainer/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.modular')
 
 @section('content')
     <div class="container">
