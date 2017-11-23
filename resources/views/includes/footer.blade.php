150be83172ce265ba1010f4a102eae95fbcb4403
Jessi <jessisibayan@gmail.com>
2017-11-23 15:34:13 +0800

    Edited footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,14 +1,14 @@
 <footer class="footer">
     <div class="footer-block buttons">
-        <iframe class="footer-github-btn" src="https://ghbtns.com/github-btn.html?user=modularcode&repo=modular-admin-html&type=star&count=true" frameborder="0" scrolling="0" width="140px" height="20px"></iframe>
+        {{--  <a href="http://bghmc.doh.gov.ph/" class="btn btn-sm btn-danger">Baguio General Hospital and Medical Center</a>  --}}
+        {{--  <iframe class="footer-github-btn" src="https://ghbtns.com/github-btn.html?user=modularcode&repo=modular-admin-html&type=star&count=true" frameborder="0" scrolling="0" width="140px" height="20px"></iframe>  --}}
     </div>
     <div class="footer-block author">
         <ul>
-            <li> created by
-                <a href="https://github.com/modularcode">ModularCode</a>
-            </li>
+            <li> developed by <strong>Jessi Sibayan</strong> </li>
             <li>
-                <a href="https://github.com/modularcode/modular-admin-html#get-in-touch">get in touch</a>
+                <a href="https://github.com/Jessi810"><i class="fa fa-github" aria-hidden="true"></i></a>
+                <a href="mailto:jessisibayan@gmail.com"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
             </li>
         </ul>
     </div>
