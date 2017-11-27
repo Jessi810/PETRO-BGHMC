f8192c9c581535b29f267dee57d2dd3decdfaf20
Jessi <jessisibayan@gmail.com>
2017-11-27 15:32:04 +0800

    Added skill controller

---
+++ app/Http/Controllers/SkillController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Skill;
+use Illuminate\Http\Request;
+
+class SkillController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $request->user()->authorizeRoles(['Admin']);
+        
+        return view('skill.create', ['trainer' => $request->get('trainer')]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->user()->authorizeRoles(['Admin']);
+        
+        $input = $request->all();
+        $skill = Skill::create($input);
+        $trainer = Trainer::find($request->trainer);
+        $skill->trainer()->associate($trainer)->save();
+        return redirect()->route('cv', ['id' => $trainer->id]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Skill $skill)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Request $request, Skill $skill)
+    {
+        $request->user()->authorizeRoles(['Admin']);
+        
+        return view('skill.edit', compact('skill'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Skill $skill)
+    {
+        $request->user()->authorizeRoles(['Admin']);
+        
+        $skill->update($request->all());
+        return redirect()->route('cv', ['id' => $skill->trainer_id])->with('success','Skill updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, Skill $skill)
+    {
+        $request->user()->authorizeRoles(['Admin']);
+        
+        $skill = Skill::find($skill->id);
+        $trainer_id = $skill->trainer_id;
+        $skill->delete();
+        return redirect()->route('cv', ['id' => $trainer_id])->with('success','Skill deleted successfully');
+    }
+}
