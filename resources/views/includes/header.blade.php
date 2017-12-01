10375476eb94d19aa3807bf1131ac02e96778a25
Jessi <jessisibayan@gmail.com>
2017-12-01 11:30:00 +0800

    Edited header links

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -29,7 +29,7 @@
     </div>  --}}
     <div class="header-block header-block-nav">
         <ul class="nav-profile">
-            <li class="notifications new">
+            {{--  <li class="notifications new">
                 <a href="" data-toggle="dropdown">
                     <i class="fa fa-bell-o"></i>
                     <sup>
@@ -84,20 +84,20 @@
                         </ul>
                     </footer>
                 </div>
-            </li>
+            </li>  --}}
             <li class="profile dropdown">
                 <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                     <div class="img" style="background-image: url('https://avatars3.githubusercontent.com/u/3959008?v=3&s=40')"> </div>
                     <span class="name"> {{ Auth::user()->name }} </span>
                 </a>
                 <div class="dropdown-menu profile-dropdown-menu" aria-labelledby="dropdownMenu1">
-                    <a class="dropdown-item" href="#">
+                    <a class="dropdown-item" href="{{ route('profile.index') }}">
                         <i class="fa fa-user icon"></i> Profile </a>
-                    <a class="dropdown-item" href="#">
+                    {{--  <a class="dropdown-item" href="#">
                         <i class="fa fa-bell icon"></i> Notifications </a>
                     <a class="dropdown-item" href="#">
                         <i class="fa fa-gear icon"></i> Settings </a>
-                    <div class="dropdown-divider"></div>
+                    <div class="dropdown-divider"></div>  --}}
                     {{--  <a class="dropdown-item" href="login.html">
                         <i class="fa fa-power-off icon"></i> Logout </a>  --}}
                     <a href="{{ route('logout') }}" class="dropdown-item"
@@ -108,7 +108,6 @@
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         {{ csrf_field() }}
                     </form>
-                    </a>
                 </div>
             </li>
         </ul>
