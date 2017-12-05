80d32d696cd2e7b3d7ea05e24a6262388cb7fbcc
Jessi <jessisibayan@gmail.com>
2017-12-05 15:23:58 +0800

    Changed theme color

--- resources/views/layouts/modular.blade.php
+++ resources/views/layouts/modular.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/vendor.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app-red.css') }}" rel="stylesheet">
     <link href="{{ asset('css/site.css') }}" rel="stylesheet">
 
     <!-- Theme initialization -->
