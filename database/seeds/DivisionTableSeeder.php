3fddad395b6c1a84055a2eb626762be973540289
Jessi <jessisibayan@gmail.com>
2017-12-13 19:01:08 +0800

    Added seed data for division and sub-division

---
+++ database/seeds/DivisionTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Petro\Division;
+use Illuminate\Database\Seeder;
+
+class DivisionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $division = new Division();
+        $division->name = 'MIS';
+        $division->save();
+        
+        $division = new Division();
+        $division->name = 'PETRO';
+        $division->save();
+    }
+}
